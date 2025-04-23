1d5107135a0d7c5abda822887e3a41732d06096a
rushcode13 <rushrushika@gmail.com>
2025-04-23 21:17:02 +0530

    Add files via upload

---
+++ intro class 1 hw.py
@@ -0,0 +1,8 @@
+print("Hi welcome to birthday teller 🎂🤩")
+yearuwereborn=input("enter the year you were born 📆:  ")
+print(" Your birth year is",yearuwereborn)
+monthuwereborn=input("\nenter the month you were born 🗓️:  ")
+print(" The month you were born in is",monthuwereborn)
+Dateuwereborn=input("\nenter the date you were born 🔢:  ")
+print(" The date you were born in is",Dateuwereborn)
+
