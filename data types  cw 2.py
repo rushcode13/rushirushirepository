2e1bb82300d5795cbc1d7b903a24fcc9c166ffd1
rushcode13 <rushrushika@gmail.com>
2025-04-23 13:48:08 +0530

    Add files via upload

---
+++ data types  cw 2.py
@@ -0,0 +1,34 @@
+#to print data type put type(.) and to get value only put the variable name.
+#in this we are printing the data type 
+a=5
+print("The data type of the value is: ",type(a))#integer
+b=4.5
+print("The data type of the value is: ",type(b))#float
+c=" Rushika Jain "
+print("The data type of the value is: ",type(c))#string
+d=True
+print("The data type of the value is: ",type(d))#boolean
+
+
+#printing the value
+weight=52.34
+weight=int(weight)
+print(weight)
+#typecasting #changing the type
+#in this case because be put int (integer) and not float the typecasting occored 
+#basically the decimal part was removed becaouse we put int and not float
+
+
+#reading the data rushika jain from the right side (so it appears reverse in the output)
+#-1 as in going right to left #negitine index
+rev=c[::-1]
+print(rev)
+
+#sum
+x=12
+y=1
+print(x+y)
+
+x=str(x)
+y=str(y)
+print(x+y)
