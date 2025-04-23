1088301e48b44351bcf8d2c9e9e4be3746c4363d
rushcode13 <rushrushika@gmail.com>
2025-04-23 21:58:16 +0530

    Add files via upload

---
+++ data types class 2 hw.py
@@ -0,0 +1,7 @@
+print("Hi!")
+achievement=input("enter a great achievment 🏆🎖️🌟:  ")
+print("\nIts great that you acheived",achievement)
+congrad=("Congradulations on ")
+achievement=str(achievement)
+print(congrad+achievement)
+
