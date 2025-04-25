9e49ca8548dfb3056dee2d5a980ffb25c6e5865d
rushcode13 <rushrushika@gmail.com>
2025-04-25 20:51:24 +0530

    Add files via upload

---
+++ operators1 class3 hw.py
@@ -0,0 +1,7 @@
+print("Hi welcome to Square root and Square calculater")
+Imput=int (input("enter number which you want to get a square of"))
+ans=Imput**2
+print(" The square root of the number you entred is",ans)
+Imput=int (input("enter number which you want to get a square root of"))
+ans=Imput**.5
+print(" The square root of the number you entred is",ans)
