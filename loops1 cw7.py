91dcb185284ecbd5d115ab39dc5a83e066edd326
rushcode13 <rushrushika@gmail.com>
2025-05-08 17:31:56 +0530

    Add files via upload

---
+++ loops1 cw7.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number whos sum you want to find"))
+sum=0
+for i in range (1,n+1):
+  sum=sum+i
+print("\nsum=",sum)
+
+for i in range(n,0,-1):
+  print(i)
---
+++ loops2 cw7.py
@@ -0,0 +1,6 @@
+string=input("please enter some text: ")
+string2=('')
+for i in string:
+    string2=i+string2
+print("\n the orignal text is: ",string)
+print("\n the reverse text is: ",string2)
