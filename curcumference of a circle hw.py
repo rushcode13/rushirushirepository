059978665afb0d4d45be02dfbfe9f02bca6e611c
rushcode13 <rushrushika@gmail.com>
2025-05-27 21:21:54 +0530

    Add files via upload

---
+++ curcumference of a circle hw.py
@@ -0,0 +1,9 @@
+print("this is a application to find the curcumference of a circle")
+radi=int(input("please enter the radius of the circle"))
+def circum():
+   pie=3.14
+   c= 2*pie*radi
+   print("the curcumference is",c)
+
+circum()
+
