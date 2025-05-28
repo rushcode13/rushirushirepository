58b5549c4392ff8469e83f957925dddfa82a8d91
rushcode13 <rushrushika@gmail.com>
2025-05-28 11:12:01 +0530

    Add files via upload

---
+++ shutdown hw for arguments.py
@@ -0,0 +1,7 @@
+def shut(choice):
+    if choice=="yes":
+      print("system shutting down")
+    else:
+       print("systen will not shut down")
+choice=(input("if you want to shut down the system type Yes or else No"))
+shut(choice)
