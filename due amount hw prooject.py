704cca66c11cf42808737997b2794b6397c3a104
rushcode13 <rushrushika@gmail.com>
2025-05-30 16:06:16 +0530

    Add files via upload

---
+++ due amount hw prooject.py
@@ -0,0 +1,6 @@
+input1=int(input("please enter how much you owe the shopkeeper"))
+input=int(input("please enter the amout of money you gave the shopkeeper"))
+def func(a,b):
+    return(a-b)
+sub=func(input,input1)
+print("the change the shopkeeper has to give you is ",sub)
