f90dd27ae948ee6276a84fa61eae16007b07f14a
rushcode13 <rushrushika@gmail.com>
2025-06-21 11:45:13 +0530

    Add files via upload

---
+++ dictionary hw.py
@@ -0,0 +1,8 @@
+code={'India':'04824',
+'Canada':'48364',
+'Nepal':'00004'}
+print("code for india is:")
+print(code.get('India','Not Found'))
+print("code for japan is:")
+print(code.get('Japan','Not Found'))
+
