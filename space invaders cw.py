f9d04ac8f40793ddd8625d28678de12c97f74e1b
rushcode13 <rushrushika@gmail.com>
2025-08-27 16:57:52 +0530

    Add files via upload

--- space invaders cw.py
+++ space invaders cw.py
@@ -65,10 +65,10 @@ running=True
 while running:
     screen.fill((0,125,0))
     screen.blit(background,(0,0))
-    for event in pygame.event.get:
+    for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-        if event.pygame==pygame.KEYDOWN:
+        if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
                 playerx_change=-5
             if event.key==pygame.K_RIGHT:
@@ -76,27 +76,27 @@ while running:
             if event.key==pygame.K_SPACE and bullet_state=="ready":
                 bulletX=playerX
                 fire_bullet(bulletX,bulletY)
-            if event.type==pygame.KEYUP and event.key in[pygame.K_LEFT,pygame.K_RIGHT]:
-                playerx_change=0
-            playerX+=playerx_change
-            playerX=max(0,min(playerX,screen_width-64))
-            for i in range(num_of_enemies):
+        if event.type==pygame.KEYUP and event.key in[pygame.K_LEFT,pygame.K_RIGHT]:
+            playerx_change=0
+    playerX+=playerx_change
+    playerX=max(0,min(playerX,screen_width-64))
+    for i in range(num_of_enemies):
                 if enemyy[i]>340:
                     for j in range(num_of_enemies):
                         enemyy[j]=2000
-                    game_over_text
+                    game_over_text()
                     break
                 enemyx[i]+=enemyx_change[i]
                 if enemyx[i]<=0 or enemyx[i]>=screen_width-64:
                     enemyx_change[i] *=-1
                     enemyy_change[i] +=enemyy_change[i]
-                if isCollision(enemyx[i],enemy[i],bulletX,bulletY):
+                if isCollision(enemyx[i],enemyy[i],bulletX,bulletY):
                     bulletY=player_start_y
                     bullet_state="ready"
                     score_value +=1
                     enemyx[i]=random.randint(0,screen_width-64)
                     enemyy[i]=random.randint(enemy_start_y_min,enemy_start_y_max)
-                enemy(enemyx[i],enemyy[i])
+                enemy(enemyx[i],enemyy[i],i)
                 if bulletY<=0:
                     bulletY=player_start_y
                     bullet_state="ready"
